97a052f818ae99aa3ebffc3b0fd8ab7a78c18534
Shubham Wagh. <shubhamwaghmare7498@gmail.com>
2025-09-28 07:33:41 +0000

    do

--- PR/do.js
+++ PR/do.js
@@ -6,3 +6,14 @@ console.log(Math.random())
 
 let date = new date()
 console.log(date.tostring())
+
+/*git add .
+git commit
+git push origin main */
+
+
+const Jsuser = {
+    name: "shubham",
+    age:69,
+    location:"jaipur"
+}
